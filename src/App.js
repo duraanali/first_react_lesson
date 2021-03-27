eaef11ed6f71b37a04a6d9b88c88fad89f88fc93
Duraan Ali <duraanali@Duraans-MacBook-Pro.local>
2021-03-27 08:45:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './index.css';
 
 function App() {
+  const initialState = 0;
+  const [count, setCount] = useState(0)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h2>React Counter</h2>
+     
+     <p className="click_desc">
+       You clicked <span>{count}</span>
+     </p>
+
+     <button onClick={ () => setCount(count + 1) }>Add 1</button>
+     <button onClick={ () => setCount(count * 5)}>Multiply by 5</button>
+     <button onClick={ () => setCount(initialState)}>Reset</button>
     </div>
   );
 }
